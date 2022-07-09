0a9e66ab0d0aeb95b072f0b56fd42896964e97c7
Ricky Gal <ricky.gal@ds2.com>
2022-07-09 18:21:29 -0500

    Initial algorithm put in place without threading or input args

--- src/MTCollatz.java
+++ src/MTCollatz.java
@@ -1,2 +1,40 @@
 public class MTCollatz {
+    public static void main(String[] args)
+    {
+        int[] stoppingTimes = new int[1000];
+        for(long i = 1; i < 100000; i++)
+        {
+            long a = i;
+            int stoppingTime = 1;
+            while (a != 1)
+            {
+                a = CalculateNextA(a);
+                stoppingTime ++;
+            }
+            stoppingTimes[stoppingTime] = stoppingTimes[stoppingTime] + 1;
+            stoppingTime = 0;
+        }
+
+        for(int i = 0; i < stoppingTimes.length; i++)
+        {
+            System.out.print(i);
+            System.out.print(",");
+            System.out.println(stoppingTimes[i]);
+        }
+    }
+
+    private static long CalculateNextA(long a)
+    {
+        long nextA;
+        if(a % 2 == 0)
+        {
+            nextA = a / 2;
+        }
+        else
+        {
+            nextA = (3*a) + 1;
+        }
+        return nextA;
+    }
+
 }
